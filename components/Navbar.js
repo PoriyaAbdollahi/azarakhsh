d2261d97eab131a02e17c8efe686c1147ae99d1b
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-04-21 19:14:53 +0430

    imporivng the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,26 +5,16 @@ const Navbar = () => {
     <>
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">Azarakash</a>
+                <a className="btn btn-ghost normal-case text-xl">آذرخش</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
-                <li><a>Item 1</a></li>
-                <li tabindex="0">
-                    <a>
-                    Parent
-                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                    </a>
-                    <ul className="p-2 bg-base-100">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                    </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                <li><a>درباره ما</a></li>
+                <li><a>ثبت سفارش</a></li>
+                <li><a>نمونه کارها</a></li>
+                <li><a>مهارت ها</a></li>
                 <li>
-                  
-                    <a className="btn bg-primary hover:bg-sky-700">Get started</a>
-
+                    <a className="btn bg-primary hover:bg-sky-700">تماس با ما</a>
                 </li>
                 </ul>
             </div>
