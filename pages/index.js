67616a3eab7b7a33f8fedd04d2a9839afdb86050
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-05 03:35:33 +0430

    technology section added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import About from "../components/About";
 import Info from "../components/Info";
 import Protfolio from "../components/Protfolio";
 import Skills from "../components/Skills";
+import Technologys from "../components/Technologys";
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto ">
@@ -9,6 +10,7 @@ export default function Home() {
     <Skills/>
     <Protfolio/>
     <Info/>
+    <Technologys/>
     </div>
   )
 }
