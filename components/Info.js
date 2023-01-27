37b39d1f2b2cf6a59aeb90c63aa150d2b89683f4
PoriyaAbdollahi <poriyaa58@gmail.com>
2023-01-27 16:40:04 +0330

    typing animation

--- components/Info.js
+++ components/Info.js
@@ -3,6 +3,7 @@ import Itguyicon from './Icons/Itguyicon'
 import Lottie from 'react-lottie';
 import animationPath from '../animation/DdmYG8H7P9.json'
 import Pencil from './Icons/Pencil';
+import { TypeAnimation } from 'react-type-animation';
 const Info = () => {
   const robot = {
     loop: true,
@@ -23,7 +24,13 @@ const Info = () => {
           />
         </div >
         <div className='w-96 md:w-full text-center '>
-          <h1 className="text-4xl font-bold text-accent "> سایتت رو به سرعت آذرخش  تحویل بگیر</h1>
+          <TypeAnimation
+            sequence={[' سایتت رو به سرعت آذرخش  تحویل بگیر',8000,'']}
+            speed={49}
+            wrapper="h1"
+            repeat={Infinity}
+            className="text-4xl font-bold text-accent "
+          />
                 <p className="py-6 text-accent">تیم ما از بهترین برنامه نویسان و طراحان وب سایت تشکیل شده </p>
           <a href='#order' className="btn btn-primary"><Pencil /> ثبت سفارش   </a>
             </div>
